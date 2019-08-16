0793c94bcb5c06beeb5b9dbd1e2ed111863a3b03
puneeth2001 <puneethchanda2001@gmail.com>
2019-08-16 22:08:13 +0530

    initial

--- test/carespace/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from graphene_django.views import GraphQLView
-from carespace.schema import schema
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema))
-]
--- test/library/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from library.models import Symptom, Disease
-# Register your models here.
-
-admin.site.register(Symptom)
-admin.site.register(Disease)
